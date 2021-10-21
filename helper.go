d759eaa53169b7c0b97248caa2daf98bf8da1249
Sky, C. Y. Lau <cylau0@gmail.com>
2021-10-21 23:46:45 +0800

    Refactoring

--- helper.go
+++ helper.go
@@ -8,10 +8,6 @@ func InterpolatePrice(t1 time.Time, v1 float64, t2 time.Time, v2 float64, tm tim
     return ( v1 * float64(tm.Sub(t1)) +  v2 * float64(t2.Sub(tm)) ) / float64(t2.Sub(t1)) 
 }
 
-func InterpolatePriceInt(t1 int64, v1 float64, t2 int64, v2 float64, tm int64) float64 {
-    return ( v1 * float64(tm - t1) + v2 * float64(t2 - tm) ) / float64(t2 - t1) 
-}
-
 func PriceTimeProductSum(t1 time.Time, v1 float64, t2 time.Time, v2 float64) float64 {
 	if t1 == t2 {
 		return float64(0.0)
@@ -19,9 +15,55 @@ func PriceTimeProductSum(t1 time.Time, v1 float64, t2 time.Time, v2 float64) flo
 	return ( v1 + v2 ) * float64(t2.Sub(t1)) / 2.0
 }
 
-func PriceTimeProductSumInt(t1 int64, v1 float64, t2 int64, v2 float64) float64 {
-	if t1 == t2 {
-		return float64(0.0)
+func CalculateAveragePrice(keys []interface{}, values []interface{}, db map[string]*MTicker, from, to time.Time) ( float64, error ) {
+	var price_from, price_to float64
+
+	TS_from := from.UnixMicro()
+	TS_to := to.UnixMicro()
+	
+    area_sum := float64(0.0)
+	started := false
+	for i, t := range keys {
+		if TS_from == t.(int64) {
+			if TS_from == TS_to {
+				return float64(0.0), nil
+			}
+			price_from = db[values[i].(string)].Price
+			started = true
+			continue
+		}
+		if i == 0 {
+			continue
+		}
+		lt := db[values[i-1].(string)]
+		ut := db[values[i].(string)]
+
+		if TS_to == t.(int64) {
+			price_to = db[values[i].(string)].Price
+			area_sum += PriceTimeProductSum(lt.Timestamp, lt.Price, ut.Timestamp, ut.Price)
+			break
+		}
+		// Special Catering
+		if !started && TS_from < t.(int64) && TS_to < t.(int64) {
+    		price_from = InterpolatePrice(lt.Timestamp, lt.Price, ut.Timestamp, ut.Price, from)
+    		price_to = InterpolatePrice(lt.Timestamp, lt.Price, ut.Timestamp, ut.Price, to)
+			return PriceTimeProductSum(from, price_from, to, price_to), nil
+		}
+		if !started && TS_from < t.(int64) {
+    		price_from = InterpolatePrice(lt.Timestamp, lt.Price, ut.Timestamp, ut.Price, from)
+			area_sum += PriceTimeProductSum(from, price_from, ut.Timestamp, ut.Price)
+			started = true
+			continue
+		}
+		if started && TS_to < t.(int64) {
+    		price_to = InterpolatePrice(lt.Timestamp, lt.Price, ut.Timestamp, ut.Price, to)
+			area_sum += PriceTimeProductSum(lt.Timestamp, lt.Price, to, price_to)
+			break;
+		}
+		if started {
+			area_sum += PriceTimeProductSum(lt.Timestamp, lt.Price, ut.Timestamp, ut.Price)
+		}
 	}
-	return ( v1 + v2 ) * float64(t2 - t1) / 2.0
+
+	return area_sum / float64(to.Sub(from)), nil
 }
--- memory_storage.go
+++ memory_storage.go
@@ -85,55 +85,5 @@ func (m *MemoryStorage) GetAveragePrice(from, to time.Time) ( float64, error ) {
 	if to.UnixMicro() > keys[len(keys)-1].(int64) {
 		return -1 , fmt.Errorf("Price is out of the data range: to = " + to.String())
 	}
-
-	var price_from, price_to float64
-
-	TS_from := from.UnixMicro()
-	TS_to := to.UnixMicro()
-	
-    area_sum := float64(0.0)
-	started := false
-	for i, t := range keys {
-		if TS_from == t.(int64) {
-			if TS_from == TS_to {
-				return float64(0.0), nil
-			}
-			price_from = m.db[values[i].(string)].Price
-			started = true
-			continue
-		}
-		if i == 0 {
-			continue
-		}
-		lt := m.db[values[i-1].(string)]
-		ut := m.db[values[i].(string)]
-
-		if TS_to == t.(int64) {
-			price_to = m.db[values[i].(string)].Price
-			area_sum += PriceTimeProductSum(lt.Timestamp, lt.Price, ut.Timestamp, ut.Price)
-			break
-		}
-		// Special Catering
-		if !started && TS_from < t.(int64) && TS_to < t.(int64) {
-    		price_from = InterpolatePrice(lt.Timestamp, lt.Price, ut.Timestamp, ut.Price, from)
-    		price_to = InterpolatePrice(lt.Timestamp, lt.Price, ut.Timestamp, ut.Price, to)
-			return PriceTimeProductSum(from, price_from, to, price_to), nil
-		}
-		if !started && TS_from < t.(int64) {
-    		price_from = InterpolatePrice(lt.Timestamp, lt.Price, ut.Timestamp, ut.Price, from)
-			area_sum += PriceTimeProductSum(from, price_from, ut.Timestamp, ut.Price)
-			started = true
-			continue
-		}
-		if started && TS_to < t.(int64) {
-    		price_to = InterpolatePrice(lt.Timestamp, lt.Price, ut.Timestamp, ut.Price, to)
-			area_sum += PriceTimeProductSum(lt.Timestamp, lt.Price, to, price_to)
-			break;
-		}
-		if started {
-			area_sum += PriceTimeProductSum(lt.Timestamp, lt.Price, ut.Timestamp, ut.Price)
-		}
-	}
-
-	return area_sum / float64(to.Sub(from)), nil
+	return CalculateAveragePrice(keys, values, m.db, from, to)
 }
